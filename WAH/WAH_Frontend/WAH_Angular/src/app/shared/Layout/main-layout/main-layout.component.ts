cc4275e10e2d1cfc11d7a0b4170d2fea1cad3e87
IPS-131 <IPS-131@IPS-131>
2025-05-27 18:12:17 +0530

    Make an asset and Inventorypage UI

---
+++ WAH/WAH_Frontend/WAH_Angular/src/app/shared/Layout/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../Component/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-main-layout',
+standalone: false,
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
