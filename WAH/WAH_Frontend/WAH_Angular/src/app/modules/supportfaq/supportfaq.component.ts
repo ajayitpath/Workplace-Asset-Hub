7b3bb9e0db40de9c6e6eeae7b5b4e59c3ba8b0a1
IPS-131 <IPS-131@IPS-131>
2025-05-27 10:54:48 +0530

    Dashboard UI

---
+++ WAH/WAH_Frontend/WAH_Angular/src/app/modules/supportfaq/supportfaq.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supportfaq',
+  imports: [],
+  templateUrl: './supportfaq.component.html',
+  styleUrl: './supportfaq.component.css'
+})
+export class SupportfaqComponent {
+
+}
