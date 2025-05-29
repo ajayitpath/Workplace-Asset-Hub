33296b2751085e1c1f5c9b7b16e7ced6f9d68163
Chaitali <chaitalis.itpath@gmail.com>
2025-05-29 12:07:46 +0530

    updated frontend

---
+++ WAH/WAH_Frontend/WAH_React/src/theme/index.js
@@ -0,0 +1,28 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // main:'rgb(85, 108, 214)' ,  // Replace with color from Figma
+      main: '#556CD6',
+    },
+    secondary: {
+      main:'#19857B' ,  // Replace with color from Figma
+    },
+    background: {
+      default:'#FFFFFF' ,  // Replace with color from Figma
+      paper: '#FFFFFF',
+    },
+    text: {
+      primary:'#000000' ,
+      secondary:'#A16C6C' ,  // Replace with color from Figma
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
+
+export default theme;
+// This theme can be used in your MUI components by wrapping your application with ThemeProvider
+// from '@mui/material/styles' and passing the theme as a prop.
